a725e4a273d9c5ed7181a1fe3bc441bdb308d421
らいぞう <144529575+raizouxyz@users.noreply.github.com>
2025-08-15 23:09:05 +0900

    Update and rename main.py to place.py

--- place.py
+++ place.py
@@ -6,10 +6,8 @@ from PIL import Image
 from capmonster_python import CapmonsterClient, TurnstileTask
 
 ##################################################
-check_mode = False
 image_path = "./images/romeda.png"
 skip_color = "#000000"
-capmonster_apikey = ""
 chunk_x = 0
 chunk_y = 0
 start_x = 0
@@ -18,18 +16,20 @@ skip_x = 0
 skip_y = 0
 ##################################################
 
-print("PlaceBot by @raizouxyz")
+print("PlaceBot(place.py) by @raizouxyz")
 print("Repository: https://github.com/raizouxyz/placebot\n")
 
+config = json.load(open("./data/config.json", "r"))
+
 proxies = []
-with open("proxies.txt", "r") as f:
+with open("./data/proxies.txt", "r") as f:
     _proxies = f.read().splitlines()
     for _proxy in _proxies:
         proxy = {"http": _proxy, "https": _proxy}
         proxies.append(proxy)
 
-with open("accounts.txt", "r") as f:
-    accounts = f.read().splitlines()
+with open("./data/tokens.txt", "r") as f:
+    tokens = f.read().splitlines()
 
 palette = {"#000000": 1,"#3c3c3c": 2,"#787878": 3,"#d2d2d2": 4,"#ffffff": 5,"#600018": 6,"#ed1c24": 7,"#ff7f27": 8,"#f6aa09": 9,"#f9dd3b": 10,"#fffabc": 11,"#0eb968": 12,"#13e67b": 13,"#87ff5e": 14,"#0c816e": 15,"#10aea6": 16,"#13e1be": 17,"#28509e": 18,"#4093e4": 19,"#60f7f2": 20,"#6b50f6": 21,"#99b1fb": 22,"#780c99": 23,"#aa38b9": 24,"#e09ff9": 25,"#cb007a": 26,"#ec1f80": 27,"#f38da9": 28,"#684634": 29,"#95682a": 30,"#f8b277": 31}
 
@@ -37,7 +37,7 @@ img = Image.open(image_path)
 rgb_img = img.convert("RGB")
 
 session = tls_client.Session(client_identifier="chrome_124",random_tls_extension_order=True)
-client = CapmonsterClient(api_key=capmonster_apikey)
+client = CapmonsterClient(api_key=config["capmonster_apikey"])
 
 def get_headers(token=None):
     headers = {
@@ -50,32 +50,10 @@ def get_headers(token=None):
 
     return headers
 
-# Check Status
-if check_mode:
-    charges_total = 0
-    for wplace_token in accounts:
-        response = session.get("https://backend.wplace.live/me", headers=get_headers(wplace_token), proxies=random.choice(proxies))
-        print(response.status_code, response.text)
-        print(response.headers)
-        if response.status_code != 200:
-            print(response.headers)
-        if response.status_code == 200:
-            user_name = response.json()['name']
-            user_id = response.json()['id']
-            charge = int(response.json()["charges"]["count"])
-            charge_max = response.json()["charges"]["max"]
-            purchasable_charges = int(response.json()["droplets"]/500)*30
-            charges_total += charge + purchasable_charges
-            print(f"[Information] {user_name}({user_id}) 配置可能:{charge}/{charge_max}(購入可能:{purchasable_charges}) Droplets:{response.json()['droplets']}")
-        else:
-            accounts.remove(wplace_token)
-    print(f"合計配置可能数:{charges_total} アカウント数:{len(accounts)}")
-    exit()
-
 nextaccount = False
 while True:
-    for wplace_token in accounts:
-        response = session.get("https://backend.wplace.live/me", headers=get_headers(wplace_token), proxies=random.choice(proxies))
+    for token in tokens:
+        response = session.get("https://backend.wplace.live/me", headers=get_headers(token), proxies=random.choice(proxies))
         if response.status_code == 200:
             droplets = response.json()["droplets"]
             charge_seconds = response.json()["charges"]["cooldownMs"] / 1000
@@ -115,38 +93,37 @@ while True:
                             time.sleep(1)
 
                         if len(request_data["colors"]) > 0:
-                            response = session.post(f"https://backend.wplace.live/s0/pixel/{chunk_x}/{chunk_y}", headers=get_headers(wplace_token), proxies=random.choice(proxies),  data=json.dumps(request_data))
+                            response = session.post(f"https://backend.wplace.live/s0/pixel/{chunk_x}/{chunk_y}", headers=get_headers(token), proxies=random.choice(proxies), data=json.dumps(request_data))
                             print(response.status_code, response.text)
                             if response.status_code == 200:
                                 painted = response.json()['painted']
-                                response = session.get("https://backend.wplace.live/me", headers=get_headers(wplace_token), proxies=random.choice(proxies))
+                                response = session.get("https://backend.wplace.live/me", headers=get_headers(token), proxies=random.choice(proxies))
                                 if response.status_code == 200:
                                     droplets = response.json()["droplets"]
                                     charge = int(response.json()["charges"]["count"])
+                                request_data = {"colors":[],"coords":[],"t":""}
+                                print(f"[Placed] 配置数:{painted} 最終座標:({start_x+x},{start_y+y}) 色:{color} 残り配置可能数:{charge}")
+                                skip_x = x + start_x
+                                skip_y = y + start_y
                             else:
-                                print(wplace_token)
-                                
-                            request_data = {"colors":[],"coords":[],"t":""}
-                            print(f"[Placed] 配置数:{painted} 最終座標:({start_x+x},{start_y+y}) 色:{color} 残り配置可能数:{charge}")
-                            skip_x = x + start_x
-                            skip_y = y + start_y
+                                print(token)
 
                             if droplets >= 500 and charge < (charge_max - 32):
-                                response = session.post("https://backend.wplace.live/purchase", headers=get_headers(wplace_token), proxies=random.choice(proxies), data=json.dumps({"product":{"id":80,"amount":1}}))
+                                response = session.post("https://backend.wplace.live/purchase", headers=get_headers(token), proxies=random.choice(proxies), data=json.dumps({"product":{"id":80,"amount":1}}))
                                 print("[Purchase] 配置可能数を購入(+30)")
                                 droplets -= 500
                                 charge += 30
                                 continue
 
                         nextaccount = True
-                        #print(f"[Charge] {charge_seconds}秒待機")
-                        #time.sleep(charge_seconds)
-                        #response = session.get("https://backend.wplace.live/me", headers=get_headers(wplace_token))
-                        #charge = int(response.json()["charges"]["count"])
+                        print(f"[Charge] {charge_seconds}秒待機")
+                        time.sleep(charge_seconds)
+                        response = session.get("https://backend.wplace.live/me", headers=get_headers(token))
+                        charge = int(response.json()["charges"]["count"])
                     if nextaccount:
                         break
                 if nextaccount:
                     break
             nextaccount = False
         else:
-            print("アホ", response.status_code, response.text)
+            print(response.status_code, response.text)
